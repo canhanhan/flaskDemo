09a05934959b87cc146ab0ed046563a580dd2fda
Can Hanhan <canhan@amazon.com>
2022-02-08 17:38:23 +0000

    Updated message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Web App with Python Flask!'
+  return 'Web App with Python Flask! New Version'
 
 app.run(host='0.0.0.0', port=3000)
 
